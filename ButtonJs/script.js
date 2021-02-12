a42e2cabb1d258f1955c549cdfd3e32dcf243cb2
Thepetapixl <apurvvidhate40@gmail.com>
2021-02-12 11:18:31 +0530

    first commit

---
+++ ButtonJs/script.js
@@ -0,0 +1,7 @@
+function buttonColorChange(){
+    document.getElementById("myHead").style.color = "green";
+}
+
+function ShowBackgroundImage() {
+    document.getElementById("background-image").style.backgroundImage = "URL('test.jpg')";
+}
