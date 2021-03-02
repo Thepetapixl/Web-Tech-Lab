699141f9f6acf60bef4ce5a8b13b66f001ea78d2
Thepetapixl <apurvvidhate40@gmail.com>
2021-03-02 10:14:59 +0530

    Commiting incase something is lost

--- Form/script.js
+++ Form/script.js
@@ -1,8 +1,19 @@
 function getData(){
-    // var FullGenderme = document.getElementById("FullGenderme").value;
-    // var Email = document.getElementById("Email").value;
-    // var PhoneNumber = document.getElementById("Phno").value;
-    // var Message = document.getElementById("Message").value;
-    var gender = document.querySelector('input[name = "gender"]:checked').value;
-    alert(gender);
-}
+    var FullName = document.getElementById("FullName").value;
+    var Email = document.getElementById("Email").value;
+    var PhoneNumber = document.getElementById("Phno").value;
+    var Message = document.getElementById("Message").value;
+    var gender = document.querySelector('input[name = "Gender"]:checked').value;
+
+    // alert(FullName);
+
+    // // var message = "Thank you for submitting this form" + FullName;
+    // // alert(message);
+}   
+
+// function getMyData(){
+//     var name = jQuery("#name").val();
+//     jQuery.load("next.html",function(){
+//     jQuery("#here").html(name);
+//     });
+// }
