c4d9ecc43bd1d28b4fa83cf975cd75663791f966
Thepetapixl <apurvvidhate40@gmail.com>
2021-03-02 11:08:23 +0530

    Linked both html files + data displayed on page 2

--- Form/script.js
+++ Form/script.js
@@ -4,16 +4,4 @@ function getData(){
     var PhoneNumber = document.getElementById("Phno").value;
     var Message = document.getElementById("Message").value;
     var gender = document.querySelector('input[name = "Gender"]:checked').value;
-
-    // alert(FullName);
-
-    // // var message = "Thank you for submitting this form" + FullName;
-    // // alert(message);
 }   
-
-// function getMyData(){
-//     var name = jQuery("#name").val();
-//     jQuery.load("next.html",function(){
-//     jQuery("#here").html(name);
-//     });
-// }
