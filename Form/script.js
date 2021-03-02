15935e023abfcd2f55936c155c0d2be09bf4ea16
Thepetapixl <apurvvidhate40@gmail.com>
2021-03-02 10:04:35 +0530

    Tried to get the radio button query; failed

---
+++ Form/script.js
@@ -0,0 +1,8 @@
+function getData(){
+    // var FullGenderme = document.getElementById("FullGenderme").value;
+    // var Email = document.getElementById("Email").value;
+    // var PhoneNumber = document.getElementById("Phno").value;
+    // var Message = document.getElementById("Message").value;
+    var gender = document.querySelector('input[name = "gender"]:checked').value;
+    alert(gender);
+}
